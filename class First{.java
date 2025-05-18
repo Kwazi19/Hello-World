5a013075c9a09f3137dc9f151b1b67665f14a42c
Kwazi19 <kwazikwakhebandlelihle@gmail.com>
2025-05-18 23:52:11 +0200

    I added a project so that I can complete it

--- class First{.java
+++ class First{.java
@@ -4,7 +4,7 @@ class First{
          
          for(int i =1; i<=num;i++) {
             if(i % 2 == 0)
-            System.out.print(i);
+            System.out.print(i + "\n");
          }
 
     }
