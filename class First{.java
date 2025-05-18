d2fa6a6c0be743e1c3bd08c04eace0c86a2d1f42
Kwazi19 <kwazikwakhebandlelihle@gmail.com>
2025-05-18 22:49:48 +0200

    This code prints out even number from this range(1-30)

---
+++ class First{.java
@@ -0,0 +1,11 @@
+class First{
+    public static void main(String[] args){
+        int num = 30;
+         
+         for(int i =1; i<=num;i++) {
+            if(i % 2 == 0)
+            System.out.print(i);
+         }
+
+    }
+}
